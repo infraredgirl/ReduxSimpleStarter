9c14250b57d7169729eb2139b1c50f9570970b13
Ana Krivokapic <akrivoka@redhat.com>
2016-06-20 13:38:44 +0200

    Books (Section 4)

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {title: 'Javascript', pages: 101},
+    {title: 'Harry Potter', pages: 600},
+    {title: 'The Dark Tower', pages: 123},
+    {title: 'Eloquent Ruby', pages:231}
+  ];
+}
